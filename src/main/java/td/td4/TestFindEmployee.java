7d46f3fb2d0f784d445bfc444dd80882de1a6871
l-gu <laurent.guer1@gmail.com>
2018-01-17 17:49:19 +0100

    records moved

--- src/main/java/td/td4/TestFindEmployee.java
+++ src/main/java/td/td4/TestFindEmployee.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.demo.persistence.impl.jpa.record.EmployeeJpaRecord;
+import td.entities.records.EmployeeJpaRecord;
 
 public class TestFindEmployee {
 	
